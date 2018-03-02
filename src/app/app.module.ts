35d7405c104265705cf4284eab1f5323e203c7e3
weeplus <weeplus@gmail.com>
2018-03-02 22:46:12 +0700

    Upload project to github

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { MatToolbarModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { ImgCardComponent } from './img-card/img-card.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImgCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatToolbarModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
